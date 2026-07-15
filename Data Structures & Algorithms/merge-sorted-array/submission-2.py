e4a97c9b31fa394457c6dc8fe8648ec92f731965
07lindanguyen <07lindanguyen@gmail.com>
2026-07-14 17:46:47 -0700

    Add: merge-sorted-array - submission-2

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # combine two arrays and sort in min-max order
+        # think about how merge sort works
+        # in-place in nums1 array
+        # Ideally in one pass
+
+        for i in range(m, m + n):
+            nums1[i] = nums2[i - m]
+        nums1.sort()
+                    
